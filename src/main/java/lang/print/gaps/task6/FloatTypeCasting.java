8194a9c88315563bfb0d2b7e1359030bb78df9fb
Husanboy <olimboyevhusanboy@gmail.com>
2022-11-21 16:35:47 +0500

    boy

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        float myDouble = 9.7f;
+        int myInt = (int) (numberToBeRounded+0.5); // Manual casting: double to int
+
+        //System.out.println(myDouble);   // Outputs 9.78
+        System.out.println(myInt);      // Outputs 9
     }
 }
--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task6;
-
+import java.util.Scanner;
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long first = 1000;
+        int second = (int) first;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
